3e796118fc0c2986f918c9cfddd16cdadf31edf5
omer358 <omermaki358@gmail.com>
2024-04-17 05:08:35 +0200

    refactor patient_display feature code

--- lib/controllers/patient_display_controller.dart
+++ lib/controllers/patient_display_controller.dart
@@ -10,45 +10,106 @@ import 'package:surgery_picker/services/firestore_service.dart';
 import '../models/patient_model.dart';
 
 class PatientDisplayController extends GetxController {
+  /// List of formatted surgery dates.
   final RxList<String> formattedDates = <String>[].obs;
+
+  /// EntryScreenController instance for accessing patient data.
   final EntryScreenController _entryScreenController =
-      Get.find<EntryScreenController>();
-  final Rx<DoctorModel> doctorModel = const DoctorModel(id: '', name: '', availableDates: []).obs;
+  Get.find<EntryScreenController>();
 
+  /// DoctorModel instance for storing doctor data.
+  final Rx<DoctorModel> doctorModel = const DoctorModel(
+    id: '',
+    name: '',
+    availableDates: [],
+  ).obs;
+
+  /// Fetches available surgery dates for the specified doctor.
+  ///
+  /// Parameters:
+  /// - [doctorName]: Name of the doctor to fetch available dates for.
+  ///
+  /// Returns: Future<void>.
+  ///
+  /// Throws: Exception if there's an error fetching data.
   Future<void> fetchAvailableSurgeryDates(String doctorName) async {
-    final listOfDocuments = await FireStoreService()
-        .getDocumentsByField(doctorsCollection, "name", doctorName);
-    var doctorMap = List.generate(
-            listOfDocuments.length, (index) => listOfDocuments[index].data())
-        .first!;
-    log(doctorMap.toString());
-    doctorModel.value = DoctorModel.fromSnapshot(listOfDocuments.first);
-    final List<dynamic> availableDates = doctorModel.value.availableDates;
+    try {
+      final listOfDocuments = await FireStoreService()
+          .getDocumentsByField(doctorsCollection, "name", doctorName);
+      final doctorData = listOfDocuments.isNotEmpty ? listOfDocuments.first.data() : null;
 
-    // Format timestamps
-    formattedDates.assignAll(availableDates.map<String>((timestamp) {
+      if (doctorData != null) {
+        doctorModel.value = DoctorModel.fromSnapshot(listOfDocuments.first);
+        final List<dynamic> availableDates = doctorModel.value.availableDates;
+        _formatDates(availableDates);
+      } else {
+        log("Doctor not found with name: $doctorName");
+      }
+    } catch (e) {
+      log("Error fetching available surgery dates: $e");
+    }
+  }
+
+  /// Formats the retrieved dates into a human-readable format.
+  ///
+  /// Parameters:
+  /// - [dates]: List of timestamps representing available surgery dates.
+  ///
+  /// Returns: void.
+  void _formatDates(List<dynamic> dates) {
+    formattedDates.assignAll(dates.map<String>((timestamp) {
       final DateTime dateTime = timestamp.toDate();
-      String formattedDateTime =
-          DateFormat('EEEE, dd MMMM - HH:mm', 'ar').format(dateTime);
-      return formattedDateTime;
+      return DateFormat('EEEE, dd MMMM - HH:mm', 'ar').format(dateTime);
     }).toList());
   }
 
+  /// Reserves a surgery appointment for the patient.
+  ///
+  /// Parameters:
+  /// - [patient]: PatientModel object representing the patient.
+  /// - [index]: Index of the selected surgery date.
+  ///
+  /// Returns: void.
   void reserveForSurgery(PatientModel patient, int index) {
+    _updatePatientSpecifiedDate(patient, index);
+    _updateDoctorAvailableDates(index);
+  }
+
+  /// Updates the patient's specified surgery date.
+  ///
+  /// Parameters:
+  /// - [patient]: PatientModel object representing the patient.
+  /// - [index]: Index of the selected surgery date.
+  ///
+  /// Returns: void.
+  void _updatePatientSpecifiedDate(PatientModel patient, int index) {
+    final String selectedDate = formattedDates[index];
     _entryScreenController.patient.value =
-        patient.copyWith(specifiedDate: formattedDates[index]);
-    FireStoreService().updateData(_entryScreenController.patient.value.toMap(),
-        patient.id, patientsCollection);
-    var remainDates = doctorModel.value.availableDates;
-    remainDates.removeAt(index);
-    log(remainDates.toString());
-    doctorModel.value = doctorModel.value.copyWith(availableDates: remainDates);
-    Map<String, List<dynamic>> updatedData = {"avilable_dates": doctorModel.value.availableDates};
+        patient.copyWith(specifiedDate: selectedDate);
+    FireStoreService().updateData(
+        _entryScreenController.patient.value.toMap(), patient.id, patientsCollection);
+  }
+
+  /// Updates the doctor's available surgery dates.
+  ///
+  /// Parameters:
+  /// - [index]: Index of the selected surgery date.
+  ///
+  /// Returns: void.
+  void _updateDoctorAvailableDates(int index) {
+    var remainingDates = doctorModel.value.availableDates;
+    remainingDates.removeAt(index);
+    doctorModel.value = doctorModel.value.copyWith(availableDates: remainingDates);
+    _updateDoctorDocument();
+  }
+
+  /// Updates the doctor document in Firestore with the new available dates.
+  ///
+  /// Returns: void.
+  void _updateDoctorDocument() {
+    Map<String, List<dynamic>> updatedData = {
+      "avilable_dates": doctorModel.value.availableDates
+    };
     FireStoreService().updateData(updatedData, doctorModel.value.id, "doctors");
-    /**
-     * fetch the doctor document based on the entered patient
-     * update the doctor document after the patient pick a surgery date, remove the selected date from the doctor document
-     *
-     */
   }
 }
--- lib/screens/patient_display_screen.dart
+++ lib/screens/patient_display_screen.dart
@@ -11,7 +11,7 @@ class PatientDisplayScreen extends StatelessWidget {
   final EntryScreenController _entryScreenController = Get.find<EntryScreenController>();
   final PatientDisplayController _patientDisplayController = Get.put(PatientDisplayController());
 
-  PatientDisplayScreen({super.key, required this.patientModel});
+  PatientDisplayScreen({Key? key, required this.patientModel}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,130 +19,88 @@ class PatientDisplayScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("بيانات المريض"),
       ),
-      body: Container(
-        padding: const EdgeInsetsDirectional.fromSTEB(8, 16, 16, 8),
-        child: Directionality(
-          textDirection: TextDirection.rtl,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                "بيانات المريض الأساسية: ",
-                style: TextStyle(fontSize: 24),
-              ),
-              Card(
-                margin: const EdgeInsets.all(8),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            "الرقم التعريفي: ",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            patientModel.id,
-                            style: const TextStyle(fontSize: 20),
-                          )
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: [
-                          const Text(
-                            "الأسم: ",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          Text(
-                            patientModel.name,
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text(
-                "معلومات التشخيص و العملية: ",
-                style: TextStyle(fontSize: 24),
-              ),
-              Card(
-                margin: const EdgeInsets.all(8),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            "العملية المحددة: ",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            patientModel.surgeryType,
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 20),
-                      Row(
-                        children: [
-                          const Text(
-                            "الطبيب: ",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Text(
-                            patientModel.doctor,
-                            style: const TextStyle(fontSize: 16),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Row(
-                        children: [
-                          const Text(
-                            "تاريخ العملية: ",
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          Obx(() {
-                            log(_entryScreenController.patient.value.toString());
-                            return Text(
-                              _entryScreenController
-                                      .patient.value.specifiedDate.isEmpty
-                                  ? "غير مُحدد"
-                                  : _entryScreenController
-                                      .patient.value.specifiedDate,
-                              style: const TextStyle(fontSize: 16),
-                            );
-                          }),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ElevatedButton(
-                onPressed: () => _showDatePicker(context),
-                child: const Text("تحديد موعد العملية"),
-              ),
-            ],
-          ),
+      body: _buildBody(context),
+    );
+  }
+
+  Widget _buildBody(BuildContext context) {
+    return Container(
+      padding: const EdgeInsetsDirectional.fromSTEB(8, 16, 16, 8),
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _buildBasicPatientData(),
+            const SizedBox(height: 20),
+            _buildDiagnosisAndOperationInfo(),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _showDatePicker(context),
+              child: const Text("تحديد موعد العملية"),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _buildBasicPatientData() {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            _buildDataRow("الرقم التعريفي:", patientModel.id),
+            const SizedBox(height: 20),
+            _buildDataRow("الأسم:", patientModel.name),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDiagnosisAndOperationInfo() {
+    return Card(
+      margin: const EdgeInsets.all(8),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            _buildDataRow("العملية المحددة:", patientModel.surgeryType),
+            const SizedBox(height: 20),
+            _buildDataRow("الطبيب:", patientModel.doctor),
+            const SizedBox(height: 20),
+            _buildOperationDate(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildOperationDate() {
+    return Row(
+      children: [
+        const Text("تاريخ العملية:", style: TextStyle(fontSize: 16)),
+        Obx(() {
+          log(_entryScreenController.patient.value.toString());
+          return Text(
+            _entryScreenController.patient.value.specifiedDate.isEmpty ? "غير مُحدد" : _entryScreenController.patient.value.specifiedDate,
+            style: const TextStyle(fontSize: 16),
+          );
+        }),
+      ],
+    );
+  }
+
+  Widget _buildDataRow(String title, String value) {
+    return Row(
+      children: [
+        Text(title, style: const TextStyle(fontSize: 20)),
+        Text(value, style: const TextStyle(fontSize: 20)),
+      ],
     );
   }
 
@@ -159,35 +117,31 @@ class PatientDisplayScreen extends StatelessWidget {
             const Divider(),
             Obx(() => _patientDisplayController.formattedDates.isEmpty
                 ? const CircularProgressIndicator()
-                : ListView.builder(
-                    itemCount: _patientDisplayController.formattedDates.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      String formattedDateTime =
-                      _patientDisplayController.formattedDates[index];
-
-                      // Assuming formattedDateTime is in the format "Month Day, Year"
-                      // You can modify this based on your actual formatted date
-                      String formattedDate = formattedDateTime
-                          .split(' - ')[0]; // Extracting the date
-
-                      // Extracting the time in hours
-                      String formattedTime =
-                          formattedDateTime.split(' - ')[1].split(' ')[0];
-                      return ListTile(
-                        title: Text(formattedDate),
-                        subtitle: Text(formattedTime),
-                        leading: const Icon(Icons.date_range),
-                        onTap: () {
-                          _patientDisplayController.reserveForSurgery(patientModel, index);
-                          Get.back();
-                        },
-                      );
-                    },
-                  )),
+                : _buildAvailableDatesList()),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildAvailableDatesList() {
+    return ListView.builder(
+      itemCount: _patientDisplayController.formattedDates.length,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        String formattedDateTime = _patientDisplayController.formattedDates[index];
+        String formattedDate = formattedDateTime.split(' - ')[0];
+        String formattedTime = formattedDateTime.split(' - ')[1].split(' ')[0];
+        return ListTile(
+          title: Text(formattedDate),
+          subtitle: Text(formattedTime),
+          leading: const Icon(Icons.date_range),
+          onTap: () {
+            _patientDisplayController.reserveForSurgery(patientModel, index);
+            Get.back();
+          },
+        );
+      },
     );
   }
 }
